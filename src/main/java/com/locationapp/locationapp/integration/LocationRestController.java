f48a652d56d1ca7df81abf33f8f4763eaf5129a2
Maxwell IG <m.imasuen-goertzen@perfect-media-solutions.de>
2024-09-28 10:08:17 +0200

    Implemented Integrated Layer consisting of Location REST API

---
+++ src/main/java/com/locationapp/locationapp/integration/LocationRestController.java
@@ -0,0 +1,47 @@
+package com.locationapp.locationapp.integration;
+
+import com.locationapp.locationapp.entity.Location;
+import com.locationapp.locationapp.repository.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/locations")
+public class LocationRestController {
+
+    @Autowired
+    LocationRepository locationRepository;
+
+    @GetMapping
+    public List<Location> getLocations()
+    {
+        return locationRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Location getLocation(@PathVariable("id") Long id)
+    {
+        return locationRepository.findById(id).get();
+    }
+
+    @PostMapping
+    public Location createLocation(@RequestBody Location location)
+    {
+        return locationRepository.save(location);
+    }
+
+    @PutMapping
+    public Location updateLocation(@RequestBody Location location)
+    {
+        return locationRepository.save(location);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteLocation(@PathVariable("id") Long id)
+    {
+        locationRepository.deleteById(id);
+    }
+
+}
